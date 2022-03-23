fc6481363b03635ba6e757a4367eeb9a50001b72
fabiotnagamine <84332616+fabiotnagamine@users.noreply.github.com>
2022-03-22 21:23:12 -0300

    Update FactorialCalculation.java

--- src/FactorialCalculation.java
+++ src/FactorialCalculation.java
@@ -1,3 +1,8 @@
+//Para esse exemplo o uso da recursividade pode até ajudar até certos valores
+//O uso exaustivo da recursividade temos o problema do StackOverFlow que é o estouro da memória da pilha
+//Nesse exemplo foi usada a iteratividade para resolver esse problema, pois a iteratividade  nesse caso 
+//são mais eficientes em termos de performance e do uso de memória
+
 import java.util.stream.LongStream;
 
 public class FactorialCalculation {
